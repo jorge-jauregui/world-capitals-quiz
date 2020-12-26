0a890f1296859f72ed13df718ba0bc64543d2097
Jorge Jauregui Gallo <62124046+jorge-jauregui@users.noreply.github.com>
2020-12-25 16:23:42 -0800

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+
+import { AppComponent } from './app.component';
+import { CountryApiService } from './country-api.service';
+import { CountryInfoComponent } from './country-info/country-info.component';
+import { MapComponent } from './map/map.component';
+import { QuizComponent } from './quiz/quiz.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { DestroyerComponent } from './destroyer/destroyer.component';
+import { ChangeModeBarComponent } from './change-mode-bar/change-mode-bar.component';
+import { TimerPipe } from './destroyer/timer.pipe';
+import { SearchComponent } from './search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Autocomplete
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CountryInfoComponent,
+    MapComponent,
+    QuizComponent,
+    HomeComponent,
+    DestroyerComponent,
+    ChangeModeBarComponent,
+    TimerPipe,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: ''
+    }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    //Autocomplete
+    MatAutocompleteModule,
+    MatInputModule,
+    FormsModule,
+  ],
+  providers: [CountryApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
